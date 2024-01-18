34e3cd6723961b5438416afe8b42cdab0ff54275
mhmdiamd <darmawanilham34@gmail.com>
2024-01-18 21:46:07 +0700

    feat: implement transport

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	"github.com/mhmdiamd/go-blockchain/network"
+)
+
+func main() {
+  trLocal := network.NewLocalTransportImpl("LOCAL")
+  trRemote := network.NewLocalTransportImpl("REMOTE")
+
+  trLocal.Connect(trRemote)
+  trRemote.Connect(trLocal)
+
+  go func() {
+    for {
+      trRemote.SendMessage(trLocal.Addr(), []byte("Hello World"))
+      time.Sleep(1 * time.Second)
+    }
+  }()
+
+  opts := network.ServerOpts{
+    Transports : []network.Transport{trLocal},
+  }
+
+  s := network.NewServer(opts)
+  s.Start()
 }
